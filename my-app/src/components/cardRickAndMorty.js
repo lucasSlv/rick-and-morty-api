abd62899e5a41bf93908cbd2b46be100d6902ebc
LucasSlv <lucassilvax005x@gmail.com>
2022-08-18 21:04:50 -0300

    commit

--- my-app/src/components/cardRickAndMorty.js
+++ my-app/src/components/cardRickAndMorty.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getRickAndMorty  } from "../services/rickAndMortyService";
+import './style.css';
 
 export const CardRickAndMorty = () => {
     const [rickAndMorty, setRickAndMorty] = useState({});
@@ -31,19 +32,19 @@ export const CardRickAndMorty = () => {
       }
     
       return (
-        <ul>
-          <li>
-            Name:{rickAndMorty.name}
-          </li>
-          <l1>
-            Status:{rickAndMorty.status}
-          </l1>
-          <li>
-            Origem:{rickAndMorty.origin.name}
-          </li>
-        </ul>
-      )
-      
-      
-      
+        <div className="c-card">
+          <ul>
+            <li>
+              Name:{rickAndMorty.name}
+            </li>
+            <l1>
+              Status:{rickAndMorty.status}
+            </l1>
+            <li>
+              Origem:{rickAndMorty.origin.name}
+            </li>
+          </ul>
+          <img src={rickAndMorty.image} alt={rickAndMorty.name} />
+        </div>
+      );      
 };
